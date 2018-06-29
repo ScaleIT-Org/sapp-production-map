b9abac1ac7d814e702e04b5544d17717efd79d6f
Kristina Katovich <kristinakatovich@gmail.com>
2018-06-29 12:58:10 +0200

    added close button in modal

--- src/pages/modal-about/modal-about.ts
+++ src/pages/modal-about/modal-about.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the ModalAboutPage page.
@@ -22,4 +22,8 @@ export class ModalAboutPage {
     console.log('ionViewDidLoad ModalAboutPage');
   }
 
+  closeModal() {
+    this.navCtrl.pop();
+
+  }
 }
