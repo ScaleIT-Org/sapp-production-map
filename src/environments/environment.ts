ae14bee602dc65836f8ba73b583fa95b4c6cc2ba
Andrei Miclaus <frostyandy2k@yahoo.com>
2018-10-18 14:30:36 +0200

    consolidation work

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   dummyData: true,
-  apiUrl: "http:///v2/keys/apps?recursive=true",
+    //apiUrl: "http:///v2/keys/apps?recursive=true",
+  apiUrl: "http://10.0.0.200:49501/v2/keys/apps?recursive=true",
   production: false
 };
